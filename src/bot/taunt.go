f8cd55afd45492a6956953da595c6c635dc11c63
proowl <pro3owl@gmail.com>
2017-08-28 15:08:02 -0400

    Support loading grammar from file

--- src/bot/taunt.go
+++ src/bot/taunt.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"regexp"
+	"io/ioutil"
+	"bufio"
+	"os"
 )
 
 func holy_grail_mentioned(input string) bool {
@@ -122,10 +126,89 @@ func Taunt(input string) string {
 	return taunt(0);
 }
 
-type TauntRules struct {
-	smth int64
+type Token struct {
+	name string
+	is_token bool
 }
 
-func LoadLangs() []TauntRules {
-	return nil
+type Grammar struct {
+	rules map[string][]Token
+}
+
+type GrammarRules struct {
+	lang_rules map[string]Grammar
+}
+
+func parse_grammar(filename string) *Grammar {
+	var grammar Grammar
+	grammar.rules = make(map[string][]Token)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("error parsing file '%s': %v\n", filename, err)
+		return nil
+	}
+	defer file.Close()
+
+	split_re := regexp.MustCompile(`::=|\|`)
+	// rule_re := regexp.MustCompile(`^([^ :=]+)\s*::=\s*([^ |]+)(:?\s*\|(:?\s*([^ |]+))+)*`)//\s*([^ |]+)(\s*|\s*[^ |]+)*`)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		splitted := split_re.Split(line, -1)
+		from := splitted[0]
+		raw_to := splitted[1:]
+		var to []Token
+		for _,val := range raw_to {
+			var istart,iend int
+			istart = 0
+			for iend = range val {
+				if val[iend] == '<' {
+					if istart < iend {
+						var token Token
+						token.name = val[istart:iend]
+						token.is_token = false
+						to = append(to, token)
+						istart = iend
+					}
+				} else if val[iend] == '>' {
+					if istart < iend {
+						var token Token
+						token.name = val[istart:iend+1]
+						token.is_token = true
+						to = append(to, token)
+						istart = iend+1
+					}
+				}
+			}
+			if istart <= iend {
+				var token Token
+				token.name = val[istart:iend+1]
+				token.is_token = false
+				to = append(to, token)
+			}
+		}
+		grammar.rules[from] = to
+	}
+
+	return &grammar
+}
+
+func LoadLangs(folder string) GrammarRules {
+	var grammars GrammarRules
+	grammars.lang_rules = make(map[string]Grammar)
+
+	grammar_file_re := regexp.MustCompile(`rules\.([a-zA-Z]+)$`)
+	files, _ := ioutil.ReadDir(folder)
+	for _, file := range files {
+		groups := grammar_file_re.FindStringSubmatch(file.Name())
+		if len(groups) > 0 {
+			lang := groups[1]
+			new_grammar := parse_grammar(folder + "/" + file.Name())
+			if new_grammar != nil {
+				grammars.lang_rules[lang] = *new_grammar
+			}
+		}
+	}
+	return grammars
 }
